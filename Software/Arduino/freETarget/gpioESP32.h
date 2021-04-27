0b6f0e9cbbd8f7898c332f2517f7a7b260836e59
James Lee <jml@jmlzone.com>
2021-04-27 17:37:51 -0400

    esp32 versions

---
+++ Software/Arduino/freETarget/gpioESP32.h
@@ -0,0 +1,79 @@
+/*
+ * ESP 32 version
+ * Global functions
+ */
+void init_gpio(void);                         // Initialize the GPIO ports
+void arm_counters(void);                      // Make the board ready
+unsigned int is_running(void);                // Return a bit mask of running sensors 
+void set_LED(unsigned int led, bool state);   // Manage the LEDs
+void set_LED(unsigned int led, uint8_t r, uint8_t g, uint8_t b);   // Manage the LED as rgb
+void set_LED(unsigned int led, uint32_t c);   // Manage the LED as rgb
+
+unsigned int read_DIP(void);                  // Read the DIP switch register
+unsigned int read_counter(unsigned int direction);
+void stop_counters(void);                     // Turn off the counter registers
+void stop_enable_counters(void);              // this HAL shoudl be added to the non esp32 version
+void read_timers(void);                       // Read and return the counter registers
+void drive_paper(void);                       // Turn on the paper motor
+void clock_start(void);
+
+/*
+ *  Port Definitions
+ */
+
+#define CTS_U       14
+#define RTS_U       27
+#define LED_PWM     26   // PWM Port
+#define LED_S        0   // pixel positions
+#define LED_X        1   // pixel positions
+#define LED_Y        2   // pixel positions
+
+#define TRIP_NORTH   0x01
+#define TRIP_EAST    0x02
+#define TRIP_SOUTH   0x04
+#define TRIP_WEST    0x08
+
+#define PAPER        21                    // Paper advance drive active low (TX1)
+#define PAPER_ON      0
+#define PAPER_OFF     1
+#define PAPER_ON_300  1
+#define PAPER_OFF_300 0
+
+#define FACE_SENSOR   4
+
+#define SPARE_1      32
+#define SPARE_2      33
+
+#define J10_1      VCC
+#define J10_2       14                    // TX3
+#define J10_3       15                    // RX3
+#define J10_4       19                    // RX1
+#define J10_5       18                    // TX1
+#define J10_6      GND
+
+#define EOF 0xFF
+
+/* 
+ I2C registers in the fpga
+ 0 RO 0x10 -- we only want to shoot 10's
+ 1 RW control 
+ 2 RO north low
+ 3 RO north high
+ 4 RO south low
+ 5 RO south hi
+ 6 RO east low
+ 7 RO east high
+ 8 RO west low
+ 9 RO west hi
+ a run status west,east,south,north
+*/
+#define FPGA_ADDR = 0x10
+#define CONTROL 1
+#define NORTH_LOW 2
+#define STATUS 0x0a
+#define DIP 0x0b
+// Bit definitions in control
+#define CLEAR 1<<0
+#define STOP  1<<1
+#define QUIET 1<<2
+#define START 1<<3
---
+++ Software/Arduino/freETarget/gpioESP32.ino
@@ -0,0 +1,365 @@
+/*-------------------------------------------------------
+ * 
+ * gpio.ino
+ * 
+ * General purpose GPIO driver
+ * 
+ * ----------------------------------------------------*/
+#include "json.h"
+#include "analog_io.h"
+#include <Adafruit_NeoPixel.h>
+#define PIXEL_PIN    36    // Digital IO pin connected to the NeoPixels.
+#define PIXEL_COUNT   3
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+struct GPIO {
+  byte port;
+  byte in_or_out;
+  byte value;
+};
+
+GPIO init_table[] = {
+  {LED_PWM,     OUTPUT, 1},
+  {RTS_U,       OUTPUT, 1},
+  {CTS_U,       INPUT_PULLUP, 0},
+
+  {FACE_SENSOR, INPUT_PULLUP, 0},
+  
+  {SPARE_1,     OUTPUT, 1},               // 18-Paper drive active low
+  {SPARE_2,     INPUT_PULLUP, 0},
+  
+  {EOF, EOF, EOF} };
+
+void face_ISR(void);
+
+/*-----------------------------------------------------
+ * 
+ * function: gpio_init
+ * 
+ * brief: Initalize the various GPIO ports
+ * 
+ * return: None
+ * 
+ *-----------------------------------------------------
+ *
+ * The GPIO programming is held in a stgrucutre and 
+ * copied out to the hardware on power up.
+ *-----------------------------------------------------*/
+
+void init_gpio(void)
+{
+  int i;
+
+  i = 0;
+  while (init_table[i].port != 0xff )
+  {
+    pinMode(init_table[i].port, init_table[i].in_or_out);
+    if ( init_table[i].in_or_out == OUTPUT )
+    {
+      digitalWrite(init_table[i].port, init_table[i].value);
+    }
+    i++;
+    strip.begin();
+    strip.clear();
+  }
+
+/*
+ * Special case of the witness paper
+ */
+  pinMode(PAPER, OUTPUT);
+  if ( revision() < REV_300 )
+  { 
+
+    digitalWrite(PAPER, PAPER_OFF);
+  }
+  else 
+  {
+    digitalWrite(PAPER, PAPER_OFF_300);
+  }
+  
+/*
+ * All done, return
+ */  
+  return;
+}
+
+
+/*-----------------------------------------------------
+   i2c wrapper functions
+   (because the 'wire library' is a bit weak)
+ *-----------------------------------------------------*/
+ byte i2c_buf[16]; // A static buffer used for I2C write and read
+ void i2c_write_reg(byte dev_addr, byte reg_addr, byte val){
+  Wire.beginTransmission(dev_addr);
+  Wire.write(reg_addr);
+  Wire.write(val);
+  Wire.endTransmission();
+}
+ void i2c_write_bytes(byte dev_addr, byte reg_addr, byte n){
+   int i;
+   Wire.beginTransmission(dev_addr);
+   Wire.write(reg_addr);
+   for(i=0; i<n;i++) {
+     Wire.write(i2c_buf[i]);
+   }
+   Wire.endTransmission();
+ }
+ byte i2c_read_reg(byte dev_addr, byte reg_addr) {
+   Wire.beginTransmission(dev_addr);
+   Wire.write(reg_addr);
+   Wire.endTransmission();
+   Wire.requestFrom(dev_addr,1);
+   return(Wire.read());
+ }
+ void i2c_read_bytes(byte dev_addr, byte reg_addr, byte n) {
+   Wire.beginTransmission(dev_addr);
+   Wire.write(reg_addr);
+   Wire.endTransmission();
+   Wire.requestFrom(dev_addr,n);
+   for(i=0; i<n;i++) {
+     i2c_buf[i] = Wire.read();
+   }
+ }
+ 
+/*-----------------------------------------------------
+ * 
+ * function: is_running
+ * 
+ * brief: Determine if the clocks are running
+ * 
+ * return: TRUE if the counters are running
+ * 
+ *-----------------------------------------------------
+ *
+ * Read in the running registers, and return a 1 for every
+ * register that is running.
+ * 
+ *-----------------------------------------------------*/
+
+unsigned int is_running (void)
+{
+  unsigned int i;
+  i = i2c_read_reg(FPGA_ADDR,STATUS) & 0x0f;
+ /*
+  *  Return the running mask
+  */
+  return i;
+  }
+
+/*-----------------------------------------------------
+ * 
+ * function: arm_counters
+ * 
+ * brief: Strobe the control lines to start a new cycle
+ * 
+ * return: NONE
+ * 
+ *-----------------------------------------------------
+ *
+ * The counters are armed by
+ * 
+ *   Stopping the current cycle
+ *   Taking the counters out of read
+ *   Making sure the oscillator is running
+ *   Clearing the counters
+ *   Enabling the counters to run again
+ * 
+ *-----------------------------------------------------*/
+void arm_counters(void)
+  {
+    i2c_write_reg(FPGA_ADDR,CONTROL,(CLEAR|STOP));
+    i2c_write_reg(FPGA_ADDR,CONTROL,0);
+/*
+ *   Attach the interrupt
+ */
+  if ( revision() >= REV_300 )
+  {
+    attachInterrupt(digitalPinToInterrupt(FACE_SENSOR),  face_ISR, CHANGE);
+  }
+  
+  return;
+  }
+
+/*
+ *  Stop the oscillator
+ */
+void stop_counters(void)
+  {
+    i2c_write_reg(FPGA_ADDR,CONTROL,(STOP|QUIET));
+  return;
+  }
+void stop_enable_counters(void)
+  {
+    i2c_write_reg(FPGA_ADDR,CONTROL,STOP);
+    i2c_write_reg(FPGA_ADDR,CONTROL, 0);
+  return;
+  }
+void start_clock(void)
+  {
+    i2c_write_reg(FPGA_ADDR,CONTROL,START);
+    i2c_write_reg(FPGA_ADDR,CONTROL, 0);
+  return;
+  }
+
+/*-----------------------------------------------------
+ * 
+ * function: read_DIP
+ * 
+ * brief: READ the jumper block setting
+ * 
+ * return: TRUE for every position with a jumper installed
+ * 
+ *-----------------------------------------------------
+ *
+ * The DIP register is read and formed into a word.
+ * The word is complimented to return a 1 for every
+ * jumper that is installed.
+ * 
+ * OR in the json_dip_switch to allow remote testing
+ * OR in  0xF0 to allow for compile time testing
+ *-----------------------------------------------------*/
+unsigned int read_DIP(void)
+{
+  unsigned int return_value;
+  
+  return_value = i2c_read_reg(FPGA_ADDR,DIP) & 0x0F;  // DIP Switch
+  return_value |= json_dip_switch;  // JSON message
+  return_value |= 0xF0;             // COMPILE TIME
+
+  return return_value;
+}  
+
+/*
+ * Turn a LED on or off
+ */
+void set_LED(unsigned int led, bool state)
+  {
+  if ( state == 0 )
+    strip.setPixelColor(led, 0,0,0);    // Off
+  else
+    strip.setPixelColor(led,127,0,0);    // ON = LOW (medium red)
+  strip.show();
+  return;  
+  }
+// overloads for setting led colors
+void set_LED(unsigned int led,uint8_t r, uint8_t g, uint8_t b) {
+  strip.setPixelColor(led,r,g,b);
+}
+void set_LED(unsigned int led, uint32_t c) {
+  strip.setPixelColor(led,c);
+}
+/*-----------------------------------------------------
+ * 
+ * function: read_timers
+ * 
+ * brief:   Read the timer registers
+ * 
+ * return:  All four timer registers read and stored
+ * 
+ *-----------------------------------------------------
+ *
+ * Force read each of the timers
+ * 
+ *-----------------------------------------------------*/
+void read_timers(void)
+{
+  i2c_read_bytes(FPGA_ADDR,NORTH_LOW,8); 
+  timer_value[N] = (i2c_buf[1] <<8) + i2c_buf[0];
+  timer_value[E] = (i2c_buf[5] <<8) + i2c_buf[4];
+  timer_value[S] = (i2c_buf[3] <<8) + i2c_buf[2];
+  timer_value[W] = (i2c_buf[7] <<8) + i2c_buf[6];
+  return;
+}
+
+/*-----------------------------------------------------
+ * 
+ * function: drive_paper
+ * 
+ * brief:    Turn on the witness paper motor for json_paper_time
+ * 
+ * return:  None
+ * 
+ *-----------------------------------------------------
+ *
+ * The function turns on the motor for the specified
+ * time.
+ * 
+ * There is a hardare change between Version 2.2 which
+ * used a transistor and 3.0 that uses a FET.
+ * The driving circuit is reversed in the two boards.
+ * 
+ *-----------------------------------------------------*/
+ void drive_paper(void)
+ {
+   if ( is_trace )
+   {
+     Serial.print("\r\nAdvancing paper...");
+   }
+    
+   if ( revision() < REV_300 )
+   {
+     digitalWrite(PAPER, PAPER_ON);          // Advance the motor drive time
+   }
+   else
+   {
+     digitalWrite(PAPER, PAPER_ON_300);          // Advance the motor drive time
+   }
+   
+   for (i=0; i != json_paper_time; i++ )
+   {
+     j = 7 * (1.0 - ((float)i / float(json_paper_time)));
+     set_LED(LED_S, j & 1);                // Show the paper advancing
+     set_LED(LED_X, j & 2);                // 
+     set_LED(LED_Y, j & 4);                // 
+     delay(PAPER_STEP);                    // in 100ms increments
+    }
+    
+   if ( revision() < REV_300 )
+   {
+     digitalWrite(PAPER, PAPER_OFF);          // Advance the motor drive time
+   }
+   else
+   {
+     digitalWrite(PAPER, PAPER_OFF_300);          // Advance the motor drive time
+     digitalWrite(PAPER, PAPER_OFF_300);          // Advance the motor drive time
+   }
+
+ /*
+  * All done, return
+  */
+  return;
+ }
+ 
+/*-----------------------------------------------------
+ * 
+ * function: face_ISR
+ * 
+ * brief:    Face Strike Interrupt Service Routint
+ * 
+ * return:   None
+ * 
+ *-----------------------------------------------------
+ *
+ * Sensor #5 is attached to the digital input #19 and
+ * is used to generate an interrrupt whenever a face
+ * strike has been detected.
+ * 
+ * The ISR simply counts the number of cycles.  Anything
+ * above 0 is an indication that sound was picked up
+ * on the front face.
+ * 
+ *-----------------------------------------------------*/
+ void face_ISR(void)
+ {
+  strike_count++;
+
+/*
+ *  Disable interrupts
+ */
+  if ( revision() >= REV_300 )
+  {
+    detachInterrupt(digitalPinToInterrupt(FACE_SENSOR));
+  }
+   return;
+ }
+ 
