9302c0fbf5942ba68921453c77b19f3e76db0428
James Lee <jml@jmlzone.com>
2021-05-08 01:01:26 -0400

    initial laypout

--- fpga/verilog/slope_det.sv
+++ fpga/verilog/slope_det.sv
@@ -1,26 +1,39 @@
+/*----------------------------------------------------------------------
+Slope detector on the front end of what coudl be a delta sigma or
+integrating adc.  
+ with 64 MHz clock use 3.3K and 200 pf cap.  
+      48 MHz clock use 3.3K and 400 pf cap.  
+ 
+ New_Value = Current_Value + (RC_Input_Value – Current_Value) * (1-exp(-T/RC)) 
+
+ the factor (1-exp(-T/RC)) is a constant calculate as 2 (0x02) so its
+ a simple shift and add 0.01565893 = 2/128 == 0.015625 
+ thus divide by 64 before adding or >>6 if you are actually integrating.
+----------------------------------------------------------------------*/
+
 module slope_det (
-  input        clk64M,
-  input        clk8M,
-  input        reset_n,
-  input        div,
-  input        comp,
-  input        slope_neg, 
-  input [5:0]  slope, 
-  output logic sdm,
-  output logic det,
-  output logic [2:0] conv
-  );
-logic [2:0]    acc;
-logic [2:0]    srLPF0;
-logic [2:0]    srLPF1;
-logic [2:0]    srLPF2;
-logic [2:0]    srLPF3;
-logic [2:0]    srLPF4;
-logic [2:0]    srLPF5;
-logic [2:0]    srLPF6;
-logic [2:0]    srLPF7;
-logic [5:0]    lpf2, lpf3, diff;
-logic 	       det_nxt;
+  input clk64M, // SD modulator and first stage at 64MHz
+  input clk8M,  // filters and down stream logic at 8MHz. Known phase relationship
+  input reset_n, 
+  input div,    // this fires when the 8MHz is divided down from the 64MHz for the know phase
+  input comp,   // input from the comparitor in the LVDS pin used as the ADC
+  input slope_neg, // for slop detection positive or negative slope
+  input [5:0] slope, // delta slope looked for (at 8mhz (0.125us) for delta t)
+  output logic sdm,  // The sdm bit back to the analog integrator
+  output logic det,  // slope detected (at 8mhz rate)
+  output logic [2:0] conv // the 3 bit adc value at 8MHz.
+); 
+logic [2:0] acc;
+logic [2:0] srLPF0;  // It would have been nice to be a 2d array but iVerilog / VCD does not handle 2d arrays.
+logic [2:0] srLPF1;
+logic [2:0] srLPF2;
+logic [2:0] srLPF3; 
+logic [2:0] srLPF4; 
+logic [2:0] srLPF5; 
+logic [2:0] srLPF6;
+logic [2:0] srLPF7; 
+logic [5:0] lpf2, lpf3, diff; 
+logic det_nxt;
 
 always_ff @(posedge clk64M or negedge reset_n)
   if(~reset_n)
